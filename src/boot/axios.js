5e46e0c991de4521f18805354e70c3dbdc8fb95b
gunawan <gunawa414@gmail.com>
2023-02-05 21:11:25 +0700

    axios

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,15 +1,9 @@
 import { boot } from 'quasar/wrappers'
 import axios from 'axios'
 
-// Be careful when using SSR for cross-request state pollution
-// due to creating a Singleton instance here;
-// If any client changes this (global) instance, it might be a
-// good idea to move this instance creation inside of the
-// "export default () => {}" function below (which runs individually
-// for each client)
-// const api = axios.create({ baseURL: 'http://localhost:5050/' })
+const api = axios.create({ baseURL: 'http://localhost:5050/' })
 // const api = axios.create({ baseURL: 'https://ambulance-blits.pptik.id:5134/' })
-const api = axios.create({ baseURL: 'http://192.168.18.4:5050' })
+// const api = axios.create({ baseURL: 'http://192.168.18.4:5050' })
 
 export default boot(({ app }) => {
   app.config.globalProperties.$axios = api
